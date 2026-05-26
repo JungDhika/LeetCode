e5eed9d104e4cc3c88c1422e0d59ac5f43747f58
JungDhika <gungdhikaaa@gmail.com>
2026-05-26 23:46:34 +0800

    add fizzbuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    
+    let number = n;
+    let output = [];
+
+    for(let i = 1; i <= number; i++){
+
+        if(i % 3 == 0 && i % 5 == 0){
+            output.push("FizzBuzz");
+        }else if(i % 3 === 0){
+            output.push("Fizz");
+        }else if( i % 5 == 0){
+            output.push("Buzz");
+        }else{
+            output.push(i.toString());
+        }
+    }
+    
+    return output;
+
+};
+
+console.log(fizzBuzz(15));
