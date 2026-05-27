9abae8396cedffbeb956f3af33899c43f05463b1
JungDhika <gungdhikaaa@gmail.com>
2026-05-27 21:54:04 +0800

    add parentheses.js

---
+++ parentheses.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+
+    // siapkan array
+    let stack = [];
+
+    // buat objek sebagai pattern
+    let map = {
+        '(' : ')',
+        '[' : ']',
+        '{' : '}'
+    }
+
+
+    // loop s menjadi char
+    for(let char of s){
+        
+        // cek apakah char sesuai dengan key di map?
+        if(map[char]){
+
+            // push ke array
+            stack.push(char);
+
+        }else{
+
+            // cek apakah ) ] } 
+            if(!stack.length) return false;
+
+            // jika map dari stack terakhir !== char
+            if(map[stack.pop()] !== char){
+                return false;
+            }
+
+        }
+
+    }
+
+    // kembalikan panjang array
+    return stack.length == 0;
+    
+};
+
+console.log(isValid("]([]){}"));
