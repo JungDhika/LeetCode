4569e1c7ea68c62d0f98e3b3aab3fab20c7bfeba
JungDhika <gungdhikaaa@gmail.com>
2026-05-28 23:28:19 +0800

    update

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -6,15 +6,18 @@ var longestCommonPrefix = function(strs) {
 
     let result = "";
 
+    // jangan biarkan string kosong
     if(strs.length == 0) return "";
     if (strs[0] === "") return "";
 
 
+    // loop semua index pertama
     for(let i = 0; i < strs[0].length; i++){
 
         let start = strs[0][i];
         let valid = true;
 
+        // loop tiap index di masing masing text
         for(let j = 1; j < strs.length; j++){
 
             if(start !== strs[j][i]){
